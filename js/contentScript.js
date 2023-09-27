847ed40fc9de3dd55d082895c1b7eb1b15915b93
Javid Asgarov <asgarov1@gmail.com>
2023-09-27 19:03:56 +0200

    fixed issue with singular "Review Tab" (some tickets have 2 Review tabs, some only one)

--- js/contentScript.js
+++ js/contentScript.js
@@ -4,6 +4,7 @@ const REVIEW_DATUM_INPUT_ID = "customfield_12201";
 const REVIEW_ANMERKUNG_TEXT_FIELD_ID = "customfield_12202";
 const REVIEWER_USERNAME = "xasgarov";
 const AKTUALISIEREN_BUTTON_ID = "edit-issue-submit";
+const REVIEW_TAB_TEXT = 'Review';
 
 chrome.runtime.onMessage.addListener((reviewLine, sender, response) => {
     review(reviewLine);
@@ -22,6 +23,11 @@ function findLinkByText(text, resultIndex=0) {
         .filter(el => el.textContent === text)[resultIndex];
 }
 
+function findAllLinksByText(text) {
+    return Array.from(document.querySelectorAll('a'))
+        .filter(el => el.textContent === text);
+}
+
 function clickId(id) {
     console.log(`About to click ${id}`)
     document.getElementById(id).click();
@@ -32,11 +38,26 @@ function getTodaysDateForReview() {
     return `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth()+1).padStart(2, '0')}.${date.getFullYear()}`
 }
 
+
+/**
+ * Some Tickets have "Review Tab" twice and in that case we want to click the second one since that is the one
+ * inside the new dialog (index 1)
+ *
+ * Some tickets only have 1 "Review Tab" and then we want to click that one (index 0)
+ */
+function clickReviewTab() {
+    if (findAllLinksByText(REVIEW_TAB_TEXT).length > 1) {
+        clickId(findLinkByText(REVIEW_TAB_TEXT, 1).id);
+    } else {
+        clickId(findLinkByText(REVIEW_TAB_TEXT, 0).id);
+    }
+}
+
 function review(reviewLine) {
     clickId(BEARBEITEN_BUTTON_ID);
 
     new Promise(resolve => setTimeout(resolve, 300))
-        .then(() => clickId(findLinkByText('Review', 1).id)) // Our Jira page has "Review" twice, second result is on the popup dialog, hence index 1
+        .then(() => clickReviewTab())
         .then(async () => await new Promise(resolve => setTimeout(resolve, 300)))
         .then(() => setInputIfExists(REVIEWER_INPUT_ID, REVIEWER_USERNAME))
         .then(() => setInputIfExists(REVIEW_DATUM_INPUT_ID, getTodaysDateForReview()))
